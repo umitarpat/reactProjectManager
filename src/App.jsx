bfe66592f6c525ddd57bf758a39ea18c4ce2cc76
Ümit Arpat <umitarpatkou@gmail.com>
2024-03-24 00:20:22 +0300

    init theme design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar />
+      <NoProjectSelected />
+    </main>
   );
 }
 
